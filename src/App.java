f4f65b12954748f3074f011bee938888a149511e
Nagy <janos@zold.lan>
2022-03-01 08:57:47 +0100

    Kezdes

---
+++ src/App.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("----------------------");
+        System.out.println();        
+        System.out.println("1) Program inditasa");
+        System.out.println();
+        System.out.println("2) Nevjegy");
+        System.out.println();
+        System.out.println("3) Kilepes");
+
+        System.out.println();
+        System.out.println();
+        System.out.print("Valasztas: ");
+        String valasztott = sc.nextLine();
+
+        
+        
+    }
+}
